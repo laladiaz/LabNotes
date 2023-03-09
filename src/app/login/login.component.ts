b6ecfe05ea63fac1b26496e71366b9b82d814675
laladiaz <mariaabelardadiaz@gmail.com>
2023-03-09 16:51:39 -0600

    adds the sign in with google functionality in login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import{ AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  constructor(
+    public afAuth: AngularFireAuth
+  ){}
+  login(){
+    this.afAuth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then((result)=>{
+      console.log('You are in!', result)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+  logout(){
+    this.afAuth.signOut();
+  }
 }
